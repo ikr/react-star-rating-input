ea2b2e874606e728b951eb7316c84d6922bd648c
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-09-19 09:59:33 +0200

    New tests structure

--- tests/StarRatingInput.spec.js
+++ tests/StarRatingInput.spec.js
@@ -1,11 +1,10 @@
-describe('StarRatingInput', function () {
+describe('StarRatingInput instance', function () {
     'use strict';
 
     var assert = require('assert'),
         jsdom = require('jsdom'),
         TestUtils = require('react/addons').addons.TestUtils,
         StarRatingInput = require('../src/StarRatingInput'),
-        component,
         $;
 
     beforeEach(function (done) {
@@ -13,7 +12,6 @@ describe('StarRatingInput', function () {
         global.window = global.document.parentWindow;
 
         jsdom.jQueryify(global.window, 'http://code.jquery.com/jquery-1.11.0.min.js', function () {
-            component = TestUtils.renderIntoDocument(StarRatingInput({}));
             $ = global.window.$;
             done();
         });
@@ -23,7 +21,15 @@ describe('StarRatingInput', function () {
         global.window.close();
     });
 
-    it('is rendered with the root element\'s class assigned', function () {
-        assert($(component.getDOMNode()).hasClass('star-rating-input'));
+    describe('static markup', function () {
+        var component;
+
+        beforeEach(function () {
+            component = TestUtils.renderIntoDocument(StarRatingInput({}));
+        });
+
+        it('has the root element\'s class assigned', function () {
+            assert($(component.getDOMNode()).hasClass('star-rating-input'));
+        });
     });
 });
