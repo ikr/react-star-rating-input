a77cb7568c31445e3fd96133f78d5d539d231a27
Ivan Krechetov <ikr@ikr.su>
2016-12-13 18:48:48 +0100

    Introduce a separate entry point for the browser

---
+++ browser.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    var StarRatingInput = require('./src/StarRatingInput'),
+        css = require('./src/css');
+
+    exports.Klass = StarRatingInput;
+    exports.css = css;
+}());
---
+++ tests/browser.spec.js
@@ -0,0 +1,24 @@
+describe('browser package index', function () {
+    'use strict';
+
+    var assert = require('assert'),
+        bro = require('jsdom-test-browser'),
+        api = require('../index'),
+        browserApi = require('../browser');
+
+    assert(bro);
+
+    describe('just as the full API', function () {
+        it('exports the component class', function () {
+            assert.strictEqual(browserApi.Klass, api.Klass);
+        });
+
+        it('exports the css', function () {
+            assert.strictEqual(browserApi.css, api.cuss);
+        });
+    });
+
+    it('exports no intlMessages', function () {
+        assert.strictEqual(typeof browserApi.intlMessages, 'undefined');
+    });
+});
