1d9d5f0719f22af3b980da5e819179ed6403ca27
Ivan Krechetov <ikr@ikr.su>
2018-07-19 11:33:50 +0200

    Extend the public API

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,12 +1,17 @@
 import * as assert from 'assert'
 import StarRatingInput from '../src/StarRatingInput'
+import StarRating from '../src/StarRating'
 import css from '../src/css'
 import * as api from '../src/index'
 
 
 describe('package index', () => {
-    it('exports the component class', () => {
+    it('exports the interactive component class', () => {
         assert.strictEqual(api.StarRatingInput, StarRatingInput)
+    })
+
+    it('exports the static component class', () => {
+        assert.strictEqual(api.StarRating, StarRating)
     })
 
     it('exports non-empty css', () => {
